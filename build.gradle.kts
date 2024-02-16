a20e69aaf925194f31338b80a760332c9089cde5
Kot512 <kelohov512@gmail.com>
2024-02-16 07:12:00 +0300

    Coroutine task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.9.21"
     application
 }
 
@@ -14,6 +14,14 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("io.reactivex.rxjava2:rxkotlin:2.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,125 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.*
+import okhttp3.*
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+private val gson = Gson()
+private val BASE_URL = "http://127.0.0.1:9999"
+private val client = OkHttpClient.Builder()
+    .connectTimeout(30, TimeUnit.SECONDS)
+    .build()
+
+fun main() {
+    with(CoroutineScope(EmptyCoroutineContext)) {
+        launch {
+            try {
+                val fullPosts = getPosts(client)
+                    .map { post ->
+                        async {
+                            FullPost(post, getComments(client, post.id), getAuthor(client, post.authorId))
+                        }
+                    }.awaitAll()
+                println(fullPosts)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+    Thread.sleep(30_000L)
 }
+
+suspend fun OkHttpClient.apiCall(url: String): Response {
+//    stopping current Coroutine / suspended fun with this one,
+//    then continuing it with parameters in Callback
+    return suspendCoroutine { continuation ->
+        Request.Builder()
+            .url(url)
+            .build()
+            .let(::newCall)
+            .enqueue(object : Callback {
+                override fun onResponse(call: Call, response: Response) {
+                    continuation.resume(response)
+                }
+
+                override fun onFailure(call: Call, e: IOException) {
+                    continuation.resumeWithException(e)
+                }
+            })
+    }
+}
+
+suspend fun <T> makeRequest(url: String, client: OkHttpClient, typeToken: TypeToken<T>): T =
+//    changing Dispatcher for function, that will be called only from Coroutines or
+//    other suspended functions
+    withContext(Dispatchers.IO) {
+        client.apiCall(url)
+            .let { response ->
+                if (!response.isSuccessful) {
+                    response.close()
+                    throw RuntimeException(response.message)
+                }
+                val body = response.body ?: throw RuntimeException("response body is null")
+                gson.fromJson(body.string(), typeToken.type)
+            }
+    }
+
+suspend fun getPosts(client: OkHttpClient): List<Post> =
+    makeRequest("$BASE_URL/api/slow/posts", client, object : TypeToken<List<Post>>() {})
+
+suspend fun getAuthor(client: OkHttpClient, authorId: Long): Author =
+    makeRequest("$BASE_URL/api/authors/$authorId", client, object : TypeToken<Author>() {})
+suspend fun getComments(client: OkHttpClient, id: Long): List<Comment> =
+    makeRequest("$BASE_URL/api/slow/posts/$id/comments", client, object : TypeToken<List<Comment>>() {})
+
+data class Post(
+    val id: Long,
+    val authorId: Long,
+    val content: String,
+    val published: Long,
+    val likedByMe: Boolean,
+    val likes: Int = 0,
+    var attachment: Attachment? = null,
+)
+
+data class FullPost(
+    val post: Post,
+    val comments: List<Comment>,
+    val author: Author
+) // TODO: ask how to do better
+
+data class FullComment(
+    val comment: Comment,
+    val author: Author
+)
+
+data class Attachment(
+    val url: String,
+    val description: String,
+    val type: AttachmentType,
+) {
+    enum class AttachmentType {
+        VIDEO, IMAGE, FILE
+    }
+}
+
+data class Comment(
+    val id: Long,
+    val postId: Long,
+    val authorId: Long,
+    val content: String,
+    val published: Long,
+    val likedByMe: Boolean,
+    val likes: Int = 0,
+)
+
+data class Author(
+    val id: Long,
+    val name: String,
+    val avatar: String,
+)
